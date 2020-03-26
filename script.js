e6e6a0f1345996a49a3eb2af766165c1ebadaad7
Akos Somodi <akossomodi@yahoo.com>
2020-03-26 11:25:59 +0100

    activate speech buttons

--- script.js
+++ script.js
@@ -71,8 +71,20 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    // Add an active effect
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  });
+
   main.appendChild(box);
 }
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
 
 // Store voices
 let voices = [];
@@ -88,6 +100,16 @@ function getVoices() {
 
     voicesSelect.appendChild(option);
   });
+}
+
+// Set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// Speak the text
+function speakText() {
+  speechSynthesis.speak(message);
 }
 
 // Voices changed
